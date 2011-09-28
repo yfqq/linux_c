47215cb5ad40f92384891a78c00675a9cc2ed4be
gwm <gusmultiracial@163.com>
2011-09-28 15:23:34 +0800

    20110928_1520

---
+++ 19_4_var/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+const int A = 10;
+int a = 20;
+static int b = 30;
+int c;
+
+int main(void)
+{
+        static int a = 40;
+        char b[] = "Hello world";
+        register int c = 50;
+
+        printf("Hello world %d\n", c);
+
+        return 0;
+}
+
